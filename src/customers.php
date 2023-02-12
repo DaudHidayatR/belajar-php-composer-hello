45d637d59e0e1f9df141fdfbc05308f683835f2e
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-02-12 20:17:00 +0700

    perbaiki bug

--- src/customers.php
+++ src/customers.php
@@ -1,10 +1,12 @@
 <?php
 namespace Daudhidayatramadhan\Belajar;
 
-class Customer{
+class Customers
+{
     public function __construct(private string $name)
     {
     }
+
     public function sayHello(string $name): string
     {
         return "hello $name, MyName is $this->name";
